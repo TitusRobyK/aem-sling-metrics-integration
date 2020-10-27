df4e9846a5f4204f20cc92d8cf6b261563a282e5
TitusRoby <titus@moonraft.com>
2020-10-27 09:49:55 +0530

    Added a session counter in Series Generator Service

--- core/src/main/java/com/aem/sling/metrics/integration/core/service/impl/SeriesGeneratorImpl.java
+++ core/src/main/java/com/aem/sling/metrics/integration/core/service/impl/SeriesGeneratorImpl.java
@@ -2,18 +2,28 @@ package com.aem.sling.metrics.integration.core.service.impl;
 
 import java.util.ArrayList;
 
+import org.apache.sling.commons.metrics.Counter;
+import org.apache.sling.commons.metrics.MetricsService;
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 
 import com.aem.sling.metrics.integration.core.service.SeriesGeneratorService;
 
 @Component(immediate = true, service = SeriesGeneratorService.class)
 public class SeriesGeneratorImpl implements SeriesGeneratorService {
 
+	@Reference
+	private MetricsService metricsService;
+
+	private Counter counter;
+
 	@Override
 	public ArrayList<Integer> gererateSeries(int firstNumber, int secondNumber, int limit) {
-
-		firstNumber = verifyAndSetParameter(firstNumber, Integer.MAX_VALUE/2, 0);
-		secondNumber = verifyAndSetParameter(secondNumber, Integer.MAX_VALUE/2, 1);
+		
+		captureMetrics();
+		
+		firstNumber = verifyAndSetParameter(firstNumber, Integer.MAX_VALUE / 2, 0);
+		secondNumber = verifyAndSetParameter(secondNumber, Integer.MAX_VALUE / 2, 1);
 		limit = verifyAndSetParameter(limit, 500, 500);
 
 		ArrayList<Integer> seriesList = new ArrayList<Integer>();
@@ -21,7 +31,7 @@ public class SeriesGeneratorImpl implements SeriesGeneratorService {
 		seriesList.add(secondNumber);
 		for (int i = 1; i <= limit; i++) {
 			int thirdNumber = firstNumber + secondNumber;
-			if(thirdNumber>=Integer.MAX_VALUE/2){
+			if (thirdNumber >= Integer.MAX_VALUE / 2) {
 				break;
 			}
 			firstNumber = secondNumber;
@@ -37,6 +47,11 @@ public class SeriesGeneratorImpl implements SeriesGeneratorService {
 		} else {
 			return number;
 		}
+	}
+
+	private void captureMetrics() {
+		counter = metricsService.counter("Series Generator Session Counter");
+		counter.increment();
 	}
 
 }
